dc9cf8c98afd4614e6c9babc5388ede7f37adf3d
netrakulkarni24-debug <netrakulkarni24@gmail.com>
2026-03-11 21:32:38 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (58.49%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,33 +1,41 @@
 class Solution {
     public void sortColors(int[] nums) {
 
-     int low=0;
-     int high=nums.length-1;
-     int i=0;
+        int zeros = 0;
+        int ones = 0;
+        int twos = 0;
 
-    while(i<=high){
-        if(nums[i]==1){
-            i++;
+        // Count 0s, 1s, 2s
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                zeros++;
+            } 
+            else if (nums[i] == 1) {
+                ones++;
+            } 
+            else {
+                twos++;
+            }
         }
-        else if(nums[i]==0){
-            int temp=nums[low];
-            nums[low]=nums[i];
-            nums[i]=temp;
-            low++;
-            i++;
-        }
-        else{
-            int temp=nums[high];
-            nums[high]=nums[i];
-            nums[i]=temp;
-            high--;
-    
 
+        int index = 0;
+
+        // Place 0s
+        for (int i = 0; i < zeros; i++) {
+            nums[index] = 0;
+            index++;
+        }
+
+        // Place 1s
+        for (int i = 0; i < ones; i++) {
+            nums[index] = 1;
+            index++;
+        }
+
+        // Place 2s
+        for (int i = 0; i < twos; i++) {
+            nums[index] = 2;
+            index++;
         }
     }
-
-       
-        
-    
-}
 }
