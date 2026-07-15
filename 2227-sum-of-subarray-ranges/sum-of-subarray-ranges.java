5d2aeb0983d12e135ce226912420612c6ea9be9d
netrakulkarni24-debug <netrakulkarni24@gmail.com>
2026-07-15 20:50:07 +0530

    Time: 19 ms (65.81%) | Memory: 47.5 MB (12.25%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,19 +1,78 @@
+import java.util.Stack;
 class Solution {
     public long subArrayRanges(int[] nums) {
-        long sum=0;
-         for(int i=0;i<nums.length;i++){
-            long largest=nums[i];
-            long  smallest= nums[i];
-            for(int j=i;j<nums.length;j++){
-                    largest= Math.max(nums[j],largest);
-                    smallest=Math.min(nums[j],smallest);
-                    sum=sum+(largest-smallest);
+        Stack <Integer> st = new Stack<>();
+      
+       long sum2=0;
+        int n = nums.length;
+        int [] nge= new int [n];
+        int [] pge= new int [n];
+        int [] pse= new int[n];
+        int[] nse= new int[n];
+       for(int i=nums.length-1;i>=0;i--){
+        while(!st.isEmpty() && nums[st.peek()]<nums[i]){
+            st.pop();
+        } 
 
-                }
-                
+        if(st.isEmpty()){
+            nge[i]=n;
+        }
+        else{
+            nge[i]=st.peek();
+        }
+       
+       st.push(i);
 
+       }
+
+       st.clear();
+
+      for(int i=0;i<nums.length;i++){
+        while(!st.isEmpty() && nums[st.peek()]<=nums[i]){
+               st.pop();
+        }
+        if(st.isEmpty()){
+            pge[i]=-1;
+        }
+        else{
+            pge[i]=st.peek();
+        }
+        st.push(i);
+      }
+
+      st.clear();
+
+      for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && nums[st.peek()] > nums[i]) {
+                st.pop();
             }
-            return sum;
+            nse[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+        st.clear();
+        
+        // Previous Smaller Element (PSE) - use >= to handle duplicates correctly
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && nums[st.peek()] >= nums[i]) {
+                st.pop();
+            }
+            pse[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+        st.clear();
+          
+          for(int i=0;i<n;i++){
+            long left= i-pge[i];
+             long right = nge[i]-i;
+              long  sum=(left*right*(long)nums[i]);
+                
+                long left1= i- pse[i];
+                long right1= nse[i]-i;
+                long sum1= (left1*right1*(long)nums[i]);
+              sum2=sum2+(sum-sum1);
 
+          }
+          return sum2;
+          
          }
-    }
+}
