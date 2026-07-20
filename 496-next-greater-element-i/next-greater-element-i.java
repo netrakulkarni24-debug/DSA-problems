3238a53be9f39628b7dda6b2c7fd59250eba3466
netrakulkarni24-debug <netrakulkarni24@gmail.com>
2026-07-20 11:00:27 +0530

    Time: 5 ms (16.08%) | Memory: 44.6 MB (94.95%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+        int n = nums2.length;
+        int[] nge = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        // Step 1: Find Next Greater Element for nums2
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!st.isEmpty() && st.peek() <= nums2[i]) {
+                st.pop();
+            }
+
+            if (st.isEmpty()) {
+                nge[i] = -1;
+            } else {
+                nge[i] = st.peek();
+            }
+
+            st.push(nums2[i]);
+        }
+
+        // Step 2: Build answer for nums1
+        int[] ans = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+
+            // Find nums1[i] in nums2
+            for (int j = 0; j < n; j++) {
+
+                if (nums1[i] == nums2[j]) {
+                    ans[i] = nge[j];
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
