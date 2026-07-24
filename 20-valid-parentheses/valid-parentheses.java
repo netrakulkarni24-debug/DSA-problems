2f68b90def9ebba4e486779754df7ab46220fe4e
netrakulkarni24-debug <netrakulkarni24@gmail.com>
2026-07-24 22:02:09 +0530

    Time: 3 ms (86.57%) | Memory: 43.2 MB (57.96%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,30 +1,25 @@
 import java.util.Stack;
-
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
-        
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            
-            if (ch == '(' || ch == '{' || ch == '[') {
-                st.push(ch);
-            } else {
-                if (st.isEmpty()) return false;
-                
-                char che = st.pop();
-                
-                // If it's NOT a matching pair, return false immediately
-                if (!((ch == ')' && che == '(') || 
-                      (ch == '}' && che == '{') || 
-                      (ch == ']' && che == '['))) {
+             Stack <Character> st = new Stack<>();
+             for(int i=0;i<s.length();i++){
+                  char ch = s.charAt(i);
+                  if(ch=='(' || ch == '{' || ch == '['){
+                    st.push(ch);
+                  }
+                  else{
+                    if(st.isEmpty())
                     return false;
-                }
-                // If it matches, do nothing and let the loop continue
-            }
-        }
-        
-        // Return true only if all brackets were properly closed
-        return st.isEmpty();
+                        
+                       char che = st.pop();
+                       if(s.charAt(i)==')' && che == '(' || s.charAt(i)=='}' && che == '{' || s.charAt(i)==']' && che == '['){
+                       continue;
+                       }
+                       else {
+                        return false ;
+                       }
+                  }
+             }
+             return st.isEmpty();
     }
 }
