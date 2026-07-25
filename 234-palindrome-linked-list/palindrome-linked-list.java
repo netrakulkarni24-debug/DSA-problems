e284e1fa4e795ddb9aeebbd8f142fd3f5d0e4e78
netrakulkarni24-debug <netrakulkarni24@gmail.com>
2026-07-25 18:55:39 +0530

    Time: 4 ms (66.01%) | Memory: 96.7 MB (28.33%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -8,26 +8,46 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
- import java.util.Stack;
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        Stack <Integer> st = new Stack<>();
-       
-        ListNode temp = head;
-        while(temp!=null){
-            st.push(temp.val);
-            temp=temp.next;
 
+    public static ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr= head;
+        while(curr!=null){
+            ListNode next = curr.next;
+            curr.next= prev;
+            prev= curr;
+            curr=next;
         }
-        temp = head;
-        while(temp!=null && (!st.isEmpty())){
-            if(st.pop()!=temp.val){
-               return false; 
-               
-            }
-            temp=temp.next;
+        return prev;
+    }
+
+    
+    public boolean isPalindrome(ListNode head) {
+        if (head==null && head.next==null)
+                  return true;
+     ListNode slow= head;
+     ListNode fast = head;
+     while(fast.next!= null  && fast.next.next!=null){
+        slow = slow.next;
+        fast = fast .next .next;
+
+     }
+
+     ListNode newhead= reverse(slow.next);
+     ListNode first = head;
+     ListNode second=newhead;
+     while(second!=null){
+        if(first.val != second.val){
+             reverse(newhead);
+             return false;
         }
-      return true; 
+        first=first.next;
+        second = second .next;
+     }
+       reverse(newhead);
+       return true;
+
         
         
     }
