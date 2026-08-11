53903898a03917e91019a6116e28a2ad4a2ef336
netrakulkarni24-debug <netrakulkarni24@gmail.com>
2026-08-11 15:42:30 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (24.85%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,15 +1,13 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int index =0;
-          HashSet <Integer> set = new LinkedHashSet<>();
-          for(int a:nums){
-            set.add(a);
-          }
-             for (int c : set){
-                nums[index]= c;
-                index++;
-             }
-       return index;
-       
+        int index = 0;
+       for(int j=1;j<nums.length;j++){
+        if(nums[index]!=nums[j]){
+             nums[index+1]=nums[j];
+             index++;
+        }
+        
+       }
+       return index+1;
     }
 }
