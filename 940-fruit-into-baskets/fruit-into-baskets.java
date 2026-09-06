ab4d146fd923af8cf123b4b0ac07a9d55a29412f
netrakulkarni24-debug <netrakulkarni24@gmail.com>
2026-09-06 14:09:16 +0530

    Time: 57 ms (22.24%) | Memory: 69.7 MB (80.83%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+          Map<Integer, Integer> basket = new HashMap<>();
+
+        // Initialize pointers and max result
+        int left = 0;
+        int maxFruits = 0;
+
+        // Traverse the fruits array using right pointer
+        for (int right = 0; right < fruits.length; right++) {
+            // Include current fruit in the map
+            basket.put(fruits[right], basket.getOrDefault(fruits[right], 0) + 1);
+
+            // If more than 2 fruit types, shrink window from left
+            while (basket.size() > 2) {
+                basket.put(fruits[left], basket.get(fruits[left]) - 1);
+
+                if (basket.get(fruits[left]) == 0) {
+                    basket.remove(fruits[left]);
+                }
+
+                left++;
+            }
+
+            // Update maximum valid window length
+            maxFruits = Math.max(maxFruits, right - left + 1);
+        }
+
+        // Return the final result
+        return maxFruits;
+
+             
+}
+}
